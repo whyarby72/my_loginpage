630e76a6a7da74b0a46d05ad39dc6ad60cdc2d79
whyarby72 <whyarby72@gmail.com>
2024-04-28 17:34:34 +0700

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Latihan Login Page',
+      title: 'Berlatih Login Page',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: LoginPage(),
     );
