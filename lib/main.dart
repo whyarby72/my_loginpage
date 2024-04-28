c7bed2fccda12001c64cd53abe3166c0950307d8
whyarby72 <whyarby72@gmail.com>
2024-04-28 17:32:22 +0700

    Rubah main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Latihan Login Page',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: LoginPage(),
     );
